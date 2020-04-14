f23dc73ef150b8e6ba2e2124cf75efb7549bb118
jacob jolsö <jacke_j_1998@hotmail.se>
2020-04-14 15:05:09 +0200

    börjar  med göra listor

--- mvc med booty/controllers/PersonController.cs
+++ mvc med booty/controllers/PersonController.cs
@@ -8,7 +8,24 @@ namespace mvc_med_booty.controllers
 {
     public class PersonController : Controller
     {
+        private readonly IPeopleService _peopleService;
+
+        public PersonController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
         public IActionResult Index()
+        {
+            return View(_peopleService.GetPeople());
+        }
+
+        public IActionResult Details(int id)
+        {
+
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
---
+++ mvc med booty/models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_med_booty.models
+{
+    public class People
+
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public bool Adress { get; set; }
+        public bool City { get; set; }
+    }
+
+}
