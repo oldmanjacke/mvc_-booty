a56816b24cf5b34bc0667d6ac2c35d397a551a39
jacob jolsö <jacke_j_1998@hotmail.se>
2020-04-29 17:02:40 +0200

    testar runt

--- mvc med booty/controllers/PersonController.cs
+++ mvc med booty/controllers/PersonController.cs
@@ -3,33 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mvc_med_booty.models;
 
 namespace mvc_med_booty.controllers
 {
     public class PersonController : Controller
     {
-        ////private readonly IPeopleService _peopleService;
+        private readonly IPeopleService _peopleService;
 
-        ////public PersonController(IPeopleService peopleService)
-        ////{
-        ////    _peopleService = peopleService;
-        ////}
+        public PersonController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
 
-        //public IActionResult Index()
-        //{
-        //    return View(_peopleService.GetPeople());
-        //}
+        public IActionResult Index()
+        {
+            return View(_peopleService.GetPeople());
+        }
 
         public IActionResult Details(int id)
         {
-           
-            return View();
+            People people = _peopleService.FindById(id);
+            if (people == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(people);
         }
 
         public IActionResult Create()
         {
-            return View();
+            
+                return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonViewModel person)//need to have zero constructor for this binding to work
+        {
+            if (ModelState.IsValid)
+            {
+                _peopleService.Create(person);
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(person);
+            }
+        }
+
         public IActionResult Edit()
         {
             return View();
