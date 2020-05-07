cd99c9c4c1a4c30790e5dffa2b5aa5da85b64d28
jacob jolsö <jacke_j_1998@hotmail.se>
2020-05-07 13:29:28 +0200

    fixat i mockpeople

--- mvc med booty/models/Repository/MockPeopleRepo.cs
+++ mvc med booty/models/Repository/MockPeopleRepo.cs
@@ -5,7 +5,59 @@ using System.Threading.Tasks;
 
 namespace mvc_med_booty.models.Repository
 {
-    public class MockPeopleRepo
+    public class MockPeopleRepo : IPeopleRepo
     {
+        private static List<People> peoples = new List<People>();
+        private static int idCounter = 0;
+
+        public List<People> All()
+        {
+            return peoples;
+        }
+
+        public People Create(People people)
+        {
+            people.Id = ++idCounter;
+            peoples.Add(people);
+            return people;
+        }
+
+        public bool Delete(int id)
+        {
+            People people = FindById(id);
+            if (people == null)
+            {
+                return false;
+            }
+            else
+            {
+                return peoples.Remove(people);
+            }
+        }
+
+        public People FindById(int id)
+        {
+            People people = peoples.SingleOrDefault(d => d.Id == id);
+
+            return people;
+        }
+
+        public bool Update(People people)
+        {
+            People original = FindById(people.Id);
+            if (people == null)
+            {
+                return false;
+            }
+            else
+            {
+                original.Name = people.Name;
+                original.Age = people.Age;
+                original.Adress = people.Adress;
+                original.City = people.City;
+
+                return true;
+            }
+        }
     }
 }
